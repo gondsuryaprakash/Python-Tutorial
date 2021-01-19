c49ed87a99bf5181777ad294c09fc10511ab351c
gondsuryaprakash <suryaprakashgond15243@gmail.com>
2021-01-19 10:50:42 +0530

    add new file

--- aaRough/rough.py
+++ aaRough/rough.py
@@ -1,12 +1,21 @@
-def swap(x,y):
-     x,y=y,x
-     print('x:{},y:{}'.format(x,y))
+def removeDuplicateUptoTwo(arr):
+     hashmap={}
+     a=[]
+     for i in range(len(arr)):
+          if arr[i] not in hashmap:
+               hashmap[arr[i]]=0
+               a.append(arr[i])
+          else:
+               if hashmap[arr[i]]<1:
+                    a.append(arr[i])
+                    hashmap[arr[i]]+=1
+     a.sort()
+     return a
 
 
-swap(5,6)
 
-
-
+a=removeDuplicateUptoTwo([1,1,1,1,2,2,2,3,3,3,5,6,7,8,4,2,1])
+print(a)
 
 
 
